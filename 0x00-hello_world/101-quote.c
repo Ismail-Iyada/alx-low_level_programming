68bfe67f600ca3061afc325be25022d4ce8a22f4
ismail IYADA <ismailiyada@gmail.com>
2023-03-16 16:07:12 -0700

    16th

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - entry
+ *
+ * some advanced stuff
+ *
+ * return: always 1 (success)
+ */
+
+int main(void)
+{
+	/**
+	 * khrat
+	 */
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
