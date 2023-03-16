403db4183888137291a2bc1817811f163deedaf4
ismail IYADA <ismailiyada@gmail.com>
2023-03-16 15:16:40 -0700

    12th

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * prints exactly a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	/**
+	 * hahahahah they driving me mad
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
