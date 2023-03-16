1c89b7bcfd77ed2518703ee1f4c1a3a33a5e4e9b
ismail IYADA <ismailiyada@gmail.com>
2023-03-16 15:23:14 -0700

    12th

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -13,6 +13,6 @@ int main(void)
 	/**
 	 * hahahahah they driving me mad
 	 */
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
