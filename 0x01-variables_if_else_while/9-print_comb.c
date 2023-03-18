f33d803dc658d769b32a7b16926035458b4ad6fd
ismail IYADA <ismailiyada@gmail.com>
2023-03-17 19:35:48 -0700

    confusion lvl 100%

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * idk what needs to be done exactly, yet
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	return (0);
+
+}
