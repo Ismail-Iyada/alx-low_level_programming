5a3951c1f13c7aae3b522f7530a01578d61d0a0c
ismail IYADA <ismailiyada@gmail.com>
2023-03-17 17:35:50 -0700

    damn it was hard to avoide the compile c90 errors

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,6 @@
 /*more headers goes there*/
 
 /*betty style doc for function main goes there*/
-
 /**
  *main - entry
  *
@@ -12,17 +11,15 @@
  *
  *Return: always 0 (success)
  */
-
 int main(void)
 {
 	int n;
+	int l;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	int a = abs(n);
-	int l = a % 10;
-
+	l = abs(n) % 10;
 	if (l > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	else if (l == 0)
